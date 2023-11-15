a47293371ad88803e575a71a8f1ed2b3a45cbbdc
smarkandu <s_markan@hotmail.com>
2023-11-15 13:25:32 -0500

    Got rid of unnecessary "covid_analysis" directory; created "src" directory and put all .py files in that

--- covid_analysis/main.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-us_vaccinations_df = pd.read_csv('./data/us_state_vaccinations.csv')
-print(len(us_vaccinations_df))
-print(us_vaccinations_df.dtypes)
-print(us_vaccinations_df.describe())
-print('\n\n')
-
